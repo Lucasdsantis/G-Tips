937f78dd9b2c484219e5a27885090dc48b144079
Lucas Santis <lucas.santis2@hotmail.com>
2022-11-20 21:03:11 -0300

    routes do forms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home/index";
 
 import { LolPage } from "./Pages/lolPage";
+import { LolForm } from "./FormPages/LolForm"
 import { TopPage } from "./LolPages/TopPage";
 import { JunglePage } from "./LolPages/JunglePage";
 import { MidPage } from "./LolPages/MidPage";
@@ -9,6 +10,7 @@ import { SupPage } from "./LolPages/SupPage";
 import { AdcPage } from "./LolPages/AdcPage";
 
 import { CsGoPage } from "./Pages/csgoPage";
+import { CsForm } from "./FormPages/CsGoForm"
 import { Dust2Page } from "./CsgoPages/Dust2Page";
 import { InfernoPage } from "./CsgoPages/InfernoPage";
 import { MiragePage } from "./CsgoPages/MiragePage";
@@ -17,6 +19,7 @@ import { TrainPage } from "./CsgoPages/TrainPage";
 import { VertigoPage } from "./CsgoPages/VertigoPage";
 
 import { FifaPage } from "./Pages/fifaPage";
+import { FifaForm } from "./FormPages/FifaForm"
 import { ModoCarreiraPage } from "./FifaPages/CarreiraPage";
 import { ProClubesPage } from "./FifaPages/ProClubsPage";
 import { UltimateTeamPage } from "./FifaPages/UltimateTeamPage";
@@ -28,6 +31,7 @@ function App() {
         <Route path="/" element={<Home />} />
 
         <Route path="/lol" element={<LolPage />} />
+        <Route path="/lolForm" element={<LolForm />} />
         <Route path="/lol-Top" element={<TopPage />} />
         <Route path="/lol-Jungle" element={<JunglePage />} />
         <Route path="/lol-Mid" element={<MidPage />} />
@@ -35,6 +39,7 @@ function App() {
         <Route path="/lol-Adc" element={<AdcPage />} />
 
         <Route path="/csgo" element={<CsGoPage />} />
+        <Route path="/csgoForm" element={<CsForm />} />
         <Route path="/csgo-Dust2Page" element={<Dust2Page />} />
         <Route path="/csgo-InfernoPage" element={<InfernoPage />} />
         <Route path="/csgo-MiragePage" element={<MiragePage />} />
@@ -43,6 +48,7 @@ function App() {
         <Route path="/csgo-VertigoPage" element={<VertigoPage />} />
 
         <Route path="/fifa" element={<FifaPage />} />
+        <Route path="/fifaForm" element={<FifaForm />} />
         <Route path="/fifa-ModoCarreiraPage" element={<ModoCarreiraPage />} />
         <Route path="/fifa-ProClubesPage" element={<ProClubesPage />} />
         <Route path="/fifa-UltimateTeamPage" element={<UltimateTeamPage />} />
