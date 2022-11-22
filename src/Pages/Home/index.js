a377fd8839598c23771a114695af4b51999be8ad
Lucas Santis <lucas.santis2@hotmail.com>
2022-11-22 17:37:21 -0300

    ultima att

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import Carousel from "react-bootstrap/Carousel";
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
-
-
-export function Home(props){
-  const {imgCsGoHP,imgLolHP, imgFifaHP} = props
+export function Home(props) {
+  const { imgCsGoHP, imgLolHP, imgFifaHP } = props;
 
   const divGeralNavbar = {
     margin: "0",
@@ -17,42 +15,74 @@ export function Home(props){
     display: "flex",
     flexDirection: "row",
     justifycontent: "space-between",
-    maxWidth: "100vw"
-    
-   
-  }
+    maxWidth: "100vw",
+  };
+
+  const gtipshome = {
+    marginLeft: "20px",
+  };
+
+  const nlstyle = {
+    marginRight: "50px",
+    marginLeft: "50px",
+  };
+
+  const aboutusnav = {
+    marginRight: "20px",
+    style: "none",
+  };
+
+  const carouselstyle = {
+    width: "100vw",
+    heigth: "500px",
+  };
 
   const gamesDiv = {
     display: "flex",
-    flexDirection: "row"
-  }
+    flexDirection: "row",
+  };
 
-  const body2 ={
+  const body2 = {
     display: "flex",
     flexDirection: "row",
-    justifycontent: "space-between",
-  }
+    justifyContent: "center",
+    backgroundColor: "#dfdbdb",
+  };
 
-  const fstyle ={
+  const fstyle = {
     backgroundColor: "grey",
-    
-  }
+    alignItems: "center",
+  };
 
-  const boxesfooter= {
+  const boxesfooter = {
     aligntext: "center",
     display: "flex",
     flexDirection: "row",
-    justifycontent: "center",
-  }
-  
-  const bStyle ={
-      width: "200px",
-      heigth: "50px",
-      fontSize: "1.3rem"
-  }
-  
-  
-  
+    justifyContent: "center",
+  };
+
+  const bStyle = {
+    width: "200px",
+    heigth: "50px",
+    fontSize: "1.3rem",
+    boderRadius: "40px",
+    marginLeft: "20px",
+    marginRight: "20px",
+  };
+
+  const body2boxes = {
+    margin: "50px",
+  };
+
+  const footerboxes = {
+    marginLeft: "200px",
+    marginRight: "200px",
+  };
+
+  const abtfooter = {
+    textAlign: "center",
+  };
+
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
@@ -61,107 +91,133 @@ export function Home(props){
 
   return (
     <>
-      
-         <Navbar bg="light" expand="lg">
-      <Container className="div-geral-navbar" style={divGeralNavbar}> 
-       <div>
-        <Navbar.Brand href="#home">G-Tips</Navbar.Brand>
-        </div>
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-        {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-       
+      <Navbar bg="light" expand="lg">
+        <Container className="div-geral-navbar" style={divGeralNavbar}>
+          <div style={gtipshome}>
+            <Navbar.Brand href="#home">G-TIPS</Navbar.Brand>
+          </div>
+          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+          {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+
           {/* <Nav className="me-auto"> */}
           <div style={gamesDiv}>
-            <Nav.Link href="../csgo">Counter Strike Global Offensive</Nav.Link>
-            <Nav.Link href="../lol">League of Legends</Nav.Link>
-            <Nav.Link href="../fifa">FIFA</Nav.Link>
-            </div>
-            <div>
-            <a href="#about">About Us</a>
-            </div>
-             {/* </Nav> */}
-        {/* </Navbar.Collapse> */}
-      </Container>
-    </Navbar>
-     
-    
-      <Carousel activeIndex={index} onSelect={handleSelect} >
+            <Nav.Link style={nlstyle} href="../csgo">
+              Counter Strike Global Offensive
+            </Nav.Link>
+            <Nav.Link style={nlstyle} href="../lol">
+              League of Legends
+            </Nav.Link>
+            <Nav.Link style={nlstyle} href="../fifa">
+              FIFA
+            </Nav.Link>
+          </div>
+          <div style={aboutusnav}>
+            <a href="#abtus">About Us</a>
+          </div>
+          {/* </Nav> */}
+          {/* </Navbar.Collapse> */}
+        </Container>
+      </Navbar>
+
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        style={carouselstyle}
+      >
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src= {imgCsGoHP}
-            alt="First slide"
-          />
-           <Carousel.Caption>
-           <Link to={"/csgo"}> <Button  style={bStyle} variant="info">Tips</Button>{' '}</Link>
-          <Button href={"https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/"} style={bStyle} variant="info">Website</Button>{' '}
+          <img className="d-block w-100" src={imgCsGoHP} alt="First slide" />
+          <Carousel.Caption>
+            <Link to={"/csgo"}>
+              {" "}
+              <Button style={bStyle} variant="info">
+                Tips
+              </Button>{" "}
+            </Link>
+            <Button
+              href={
+                "https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/"
+              }
+              style={bStyle}
+              variant="info"
+            >
+              Website
+            </Button>{" "}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={imgLolHP}
-          />
+          <img className="d-block w-100" src={imgLolHP} />
 
           <Carousel.Caption>
-          <Link to={"/lol"}><Button style={bStyle}  variant="info">Tips</Button>{' '}</Link>
-          <Button href={"https://www.leagueoflegends.com/pt-br/"} style={bStyle} variant="info">Website</Button>{' '}
-            
+            <Link to={"/lol"}>
+              <Button style={bStyle} variant="info">
+                Tips
+              </Button>{" "}
+            </Link>
+            <Button
+              href={"https://www.leagueoflegends.com/pt-br/"}
+              style={bStyle}
+              variant="info"
+            >
+              Website
+            </Button>{" "}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={imgFifaHP}
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={imgFifaHP} alt="Third slide" />
 
           <Carousel.Caption>
-          <Link to={"/fifa"}><Button style={bStyle}  variant="info">Tips</Button>{' '}</Link>
-          <Button href={"https://www.ea.com/pt-br/games/fifa/fifa-23"} style={bStyle} variant="info">Website</Button>{' '}
-
-           
+            <Link to={"/fifa"}>
+              <Button style={bStyle} variant="info">
+                Tips
+              </Button>{" "}
+            </Link>
+            <Button
+              href={"https://www.ea.com/pt-br/games/fifa/fifa-23"}
+              style={bStyle}
+              variant="info"
+            >
+              Website
+            </Button>{" "}
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
 
-      
       <div style={body2}>
-        <div>
-        <h3>What it is?</h3>
-        <p>This website is a forum for tips for the most played games in the world
-
-</p>
+        <div style={body2boxes}>
+          <h3>What it is?</h3>
+          <p>
+            This website is a forum for tips for the most played games in the
+            world
+          </p>
         </div>
 
-        <div>
-        <h3>Why use it?</h3>
-        <ul>
-          <li>Improve your Gameplay</li>
-          <li>Share your Gameplay tips</li>
-          <li>Interact with the gaming community</li>
-        </ul>
+        <div style={body2boxes}>
+          <h3>Why use it?</h3>
+          <ul>
+            <li>Improve your Gameplay</li>
+            <li>Share your Gameplay tips</li>
+            <li>Interact with the gaming community</li>
+          </ul>
         </div>
       </div>
-     
 
-     <footer style={fstyle}>
-      <div >
-        <h2>About Us</h2>
-        <div style={boxesfooter}>
+      <footer style={fstyle}>
         <div>
-          <h6>Joey Quadros</h6>
-          <p>ljlkjkljkj</p>
+          <h4 id="abtus" style={abtfooter}>
+            About Us
+          </h4>
+          <div style={boxesfooter}>
+            <div style={footerboxes}>
+              <h6>Joey Quadros</h6>
+              <p>ljlkjkljkj</p>
+            </div>
+            <div style={footerboxes}>
+              <h6>Lucas de Santis</h6>
+              <p>ljlkjkljkj</p>
+            </div>
+          </div>
         </div>
-          <div>
-          <h6>Lucas de Santis</h6>
-          <p>ljlkjkljkj</p>
-          </div>
-          </div>
-
-      </div>
-     </footer>
-
+      </footer>
     </>
   );
 }
