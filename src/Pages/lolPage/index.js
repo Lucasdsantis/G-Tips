af55e1738339c6304dcca8bdd419e8e82aa18cd3
Joey Quadros <joey_qpb@hotmail.com>
2022-11-24 15:44:31 -0300

    css all pages, like logic all pages(1bug nan)

--- src/Pages/lolPage/index.js
+++ src/Pages/lolPage/index.js
@@ -22,9 +22,14 @@ export function LolPage() {
 
   const divMae = {
     backgroundColor: "#808080",
-    padding: "1.3rem",
+    padding: "1rem",
+    width: "100%",
     border: "solid 1px black",
     borderRadius: "10px",
+    height: "90%",
+    overflow: "scroll",
+    overflowX: "hidden",
+    overflowY: "auto",
   };
 
   const divTitle = {
